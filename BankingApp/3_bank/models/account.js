50484f6c92d7c56a94eb5dde42380d3622cb1528
Christopher André <55132343+ChrisMJAndre@users.noreply.github.com>
2020-11-06 14:14:23 +0100

    made the new schema as instructed in exam sheet

--- BankingApp/3_bank/models/account.js
+++ BankingApp/3_bank/models/account.js
@@ -2,6 +2,31 @@ const mongoose = require("mongoose");
 
 // 3. Account Schema
 const AccountSchema = new mongoose.Schema({
+  id: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
+  client_id: {
+    type: String,
+    required: true,
+  },
+  balance: {
+    type: String,
+    required: true,
+  },
+  alias: {
+    type: String,
+    required: true,
+  },
+});
+
+const model = mongoose.model("Account", AccountSchema);
+
+const ClientSchema = new mongoose.Schema({
+  id: {
+    type: Number,
+    required: true,
+  },
   firstName: {
     type: String,
     required: true,
@@ -10,16 +35,13 @@ const AccountSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  balance: {
-    type: Number,
-    required: true,
-  },
-  branch: {
+  street_address: {
     type: String,
     required: true,
   },
 });
 
-const model = mongoose.model("Account", AccountSchema);
+const model2 = mongo.model2("Client", ClientSchema);
 
 module.exports = model;
+module.exports = model2;
