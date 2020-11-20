087820a0169df13c386eb2faf875a206e6ec7be5
Christopher André <55132343+ChrisMJAndre@users.noreply.github.com>
2020-11-20 13:52:11 +0100

    final implementation of ssl and loadbalancer

--- BankingApp/3_bank/app.js
+++ BankingApp/3_bank/app.js
@@ -9,28 +9,14 @@ const https = require("https");
 var seaport = require("seaport");
 var sp = seaport.connect("localhost", 9090);
 
-//Load Balancer
-let pings = 0;
-var server = https.createServer(function (req, res) {
-  pings++;
-  res.end(`Port: ` + this.address().port);
-  console.log("Server A responded to request " + pings);
-});
-
-server.listen(sp.register("server"), function () {
-  console.log("Server A is listening on port: " + this.address().port);
-});
-
 //curl http://localhost:8080
 //Added Json Body-parser
 app.use(bodyParser.json());
 
 //Import Routes
 const accountRoute = require("./routes/accounts");
-const routerAccount = require("./routes/accounts");
 
 const clientRoute = require("./routes/clients");
-const routerClient = require("./routes/clients");
 
 app.use("/accounts", accountRoute);
 app.use("/client", clientRoute);
@@ -41,9 +27,7 @@ const sslServer = https.createServer(
     key: fs.readFileSync(path.join(__dirname, "cert", "key.pem")),
     cert: fs.readFileSync(path.join(__dirname, "cert", "cert.pem")),
   },
-  app,
-  routerAccount,
-  routerClient
+  app
 );
 
 //Initial route
@@ -53,9 +37,9 @@ app.get("/", (req, res) => {
 
 //Start listening
 
-sslServer.listen(3443, () => {
+sslServer.listen(sp.register("server"), function () {
   db.getConnection().then(() => {
     console.log("Database connected");
   });
-  console.log("server is listening on 3443");
+  console.log("server is listening on " + this.address().port);
 });
--- BankingApp/3_bank/loadBalancer/load-balancer.js
+++ BankingApp/3_bank/loadBalancer/load-balancer.js
@@ -2,22 +2,34 @@ var https = require("https");
 var httpProxy = require("http-proxy");
 var seaport = require("seaport");
 var sp = seaport.connect("localhost", 9090);
+const path = require("path");
+const fs = require("fs");
 
 // Reference til step 4
 var i = -1;
 
 var proxy = httpProxy.createProxyServer({});
 
-var server = https.createServer(function (req, res) {
-  var addresses = sp.query();
-  if (addresses.length == 0) {
-    res.end("No Servers Available");
+var server = https.createServer(
+  {
+    key: fs.readFileSync(path.join(__dirname, "../cert", "key.pem")),
+    cert: fs.readFileSync(path.join(__dirname, "../cert", "cert.pem")),
+  },
+  function (req, res) {
+    var addresses = sp.query();
+    if (addresses.length == 0) {
+      res.end("No Servers Available");
+    }
+    // Reference til step 4
+    i = (i + 1) % addresses.length;
+    var host = addresses[i].host.split(":").reverse()[0];
+    var port = addresses[i].port;
+    console.log(port);
+    proxy.web(req, res, {
+      target: "https://" + host + ":" + port,
+      secure: false,
+    });
   }
-  // Reference til step 4
-  i = (i + 1) % addresses.length;
-  var host = addresses[i].host.split(":").reverse()[0];
-  var port = addresses[i].port;
-  proxy.web(req, res, { target: "https://" + host + ":" + port });
-});
+);
 
 server.listen(8080, "localhost");
