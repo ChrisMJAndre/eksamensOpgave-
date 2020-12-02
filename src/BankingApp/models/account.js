ce375528699c17ffcb14ac35643d9349323300bb
ChrisMJAndre <chan19af@student.cbs.dk>
2020-12-02 15:25:13 +0100

    added version key

--- src/BankingApp/models/account.js
+++ src/BankingApp/models/account.js
@@ -1,20 +1,23 @@
 const mongoose = require("mongoose");
 
 // Creating Account Schema
-const AccountSchema = new mongoose.Schema({
-  client_id: {
-    type: String,
-    required: true,
+const AccountSchema = new mongoose.Schema(
+  {
+    client_id: {
+      type: String,
+      required: true,
+    },
+    balance: {
+      type: Number,
+      required: true,
+    },
+    alias: {
+      type: String,
+      required: true,
+    },
   },
-  balance: {
-    type: Number,
-    required: true,
-  },
-  alias: {
-    type: String,
-    required: true,
-  },
-});
+  { versionKey: false }
+);
 
 // Exporting Account Schema
 const model = mongoose.model("Account", AccountSchema);
